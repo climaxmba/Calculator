965acabdf0718684707eab481ebc2fc62028eacb
Climax Mba <climaxium150@gmail.com>
2023-03-15 05:25:08 +0100

    Declare operate()

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function divide(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        default:
+            break;
+    }
+}
