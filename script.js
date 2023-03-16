da59b4589bed4470b76d542a2f4ce7391f66d4c9
Climax Mba <climaxium150@gmail.com>
2023-03-16 14:38:54 +0100

    Add number & operator event listeners

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+const display1 = document.getElementById('display-1');
+const display2 = document.getElementById('display-2');
+const numbers = document.querySelectorAll('.numbers');
+const operators = document.querySelectorAll('.operators');
+const signs = '+-÷×';
+let currDisplay1 = '';
+let ans;
+
+ready();
+
+function ready() {
+    clearDisplay();
+
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', () => updateDisplay1(numbers[i].textContent));
+    }
+    for (let i = 0; i < operators.length; i++) {
+        operators[i].onclick = () => {
+            if (!signs.includes(display1.textContent.slice(-1))) {
+                updateDisplay1(operators[i].textContent);
+            } else {
+                display1.textContent = display1.textContent.slice(0, -1);
+                updateDisplay1(operators[i].textContent);
+            }
+        }
+    }
+}
+
+function clearDisplay() {
+    display1.textContent = '';
+    display2.textContent = '';
+}
+
+function updateDisplay1(btn, del) {
+    if (!del) {
+        display1.textContent += btn;  
+    } else (
+        display1.textContent = display1.textContent.slice(0, -1)
+    )
+    currDisplay1 = display1.textContent;
+}
+
 function add(a, b) {
     return a + b;
 }
