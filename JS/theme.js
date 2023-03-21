b24b9df7a930c720180f67d11f8ce51d4f93d149
Climax Mba <climaxium150@gmail.com>
2023-03-21 13:06:41 +0100

    Modify JS/theme.js & index.html for theme

--- JS/theme.js
+++ JS/theme.js
@@ -1,1 +1,17 @@
-const theme = document.querySelector('.material-symbols-outlined');
+const themeBtn = document.querySelector('.material-symbols-outlined');
+const themeStyle = document.getElementById('theme');
+
+
+themeBtn.onclick = () => {
+    if (themeBtn.textContent == 'light_mode') {
+        themeStyle.innerHTML = "body {background-color: #2f353b;}"
+        + "#calc {background-color: #505a64; border: 1px outset white;}"
+        + "#calc-display-container {color: white; background-color: #41534a; border: 1px outset white;}"
+        + ".calc-display::-webkit-scrollbar-thumb {background-color: #586e63;}"
+
+        themeBtn.textContent = 'dark_mode';
+    } else {
+        themeStyle.innerHTML = "";
+        themeBtn.textContent = 'light_mode';
+    }
+}
