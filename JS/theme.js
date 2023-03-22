18165e40217170778b718acfdacadfcae94f5a72
Climax Mba <climaxium150@gmail.com>
2023-03-22 06:05:18 +0100

    Modify border & add media queries

--- JS/theme.js
+++ JS/theme.js
@@ -5,8 +5,8 @@ const themeStyle = document.getElementById('theme');
 themeBtn.onclick = () => {
     if (themeBtn.textContent == 'light_mode') {
         themeStyle.innerHTML = "body {background-color: #2f353b;}"
-        + "#calc {background-color: #505a64; border: 1px outset white;}"
-        + "#calc-display-container {color: white; background-color: #41534a; border: 1px outset white;}"
+        + "#calc {background-color: #505a64; border: 0.5px outset white;}"
+        + "#calc-display-container {color: white; background-color: #41534a; border: 0.5px outset white;}"
         + ".calc-display::-webkit-scrollbar-thumb {background-color: #586e63;}"
 
         themeBtn.textContent = 'dark_mode';
