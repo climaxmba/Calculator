192f3dd58922c16925e0734a70e79935eaab97f6
Climax Mba <climaxium150@gmail.com>
2023-03-23 19:51:07 +0100

    Modify dark theme

--- JS/theme.js
+++ JS/theme.js
@@ -6,7 +6,7 @@ themeBtn.onclick = () => {
     if (themeBtn.textContent == 'light_mode') {
         themeStyle.innerHTML = "body {background-color: #2f353b;}"
         + "#calc {background-color: #505a64; border: 0.5px outset white;}"
-        + "#calc-display-container {color: white; background-color: #41534a; border: 0.5px outset white;}"
+        + "#calc-display-container {color: white; background-color: #424b53; border: 0.5px solid white;}"
         + ".calc-display::-webkit-scrollbar-thumb {background-color: #586e63;}"
 
         themeBtn.textContent = 'dark_mode';
